816ffa0962a4dc851c3dcbf9b35126164951d274
ssjgud12 <150245254+ssjgud12@users.noreply.github.com>
2026-09-18 10:38:12 +0000

    Typo with function name

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI(title="Lab1 - Fastapi User Api")
 @app.get("/health")
 def health():
     return{"status": "ok"}
+
+@app.get("/hello")
+def hello():
+    return{"status": "Good Morning World"}
