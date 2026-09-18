a2d4f9101e75e1accb28c3974d09630c96400b14
ssjgud12 <150245254+ssjgud12@users.noreply.github.com>
2026-09-18 11:12:02 +0000

    Fixed UserCreate()

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
-from fastapi import FastAPI, HTTPException, status
-from app.schemas import UserCreate
-
-app = FastAPI(title="Lab1 - Fastapi User Api")
-
-
-users: list[UserCreate] = []
-@app.get("/health")
-def health():
-    return{"status": "ok"}
-
-@app.get("/hello")
-def hello():
-    return{"status": "Good Morning World"}
-
-@app.post("api/users", status_code=status.HTTP_201_CREATED)
-def add_user(new_user: UserCreate):
-    for existing_user in users:
-        if existing_user.userid == new_user.userid:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                detail="A user with id already exists")
-    users.append(new_user)
+from fastapi import FastAPI, HTTPException, status 
+ 
+from app.schemas import UserCreate 
+ 
+app = FastAPI(title="Lab 1 - FastAPI User API") 
+ 
+users: list[UserCreate] = [] 
+ 
+ 
+@app.get("/health") 
+def health(): 
+    return {"status": "ok"} 
+ 
+ 
+@app.get("/hello") 
+def hello(): 
+    return {"message": "Hello from FastAPI"} 
+ 
+ 
+@app.post("/api/users", status_code=status.HTTP_201_CREATED) 
+def add_user(new_user: UserCreate): 
+    for existing_user in users: 
+        if existing_user.user_id == new_user.user_id: 
+            raise HTTPException( 
+                status_code=status.HTTP_409_CONFLICT, 
+                detail="A user with this user_id already exists", 
+            ) 
+ 
+    users.append(new_user) 
     return new_user
-    
