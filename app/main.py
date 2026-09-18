5f8c359a31103f9b45ff4b9f66975dfac72f8aee
ssjgud12 <150245254+ssjgud12@users.noreply.github.com>
2026-09-18 13:21:00 +0000

    Fixed add user used regular brakcets instead of curly

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def health():
 @app.get("/hello") 
 def hello(): 
     return {"message": "Hello from FastAPI"} 
- 
+
 
 @app.post("/api/users", status_code=status.HTTP_201_CREATED) 
 def add_user(new_user: UserCreate): 
@@ -28,21 +28,9 @@ def add_user(new_user: UserCreate):
  
     users.append(new_user) 
     return new_user
+ 
+    
+ 
 
-@app.get("/api/users") 
-def get_users(): 
-    return users 
- 
- 
-@app.get("/api/users/{user_id}") 
-def get_user(user_id: int): 
-    for existing_user in users: 
-        if existing_user.user_id == user_id: 
-            return existing_user 
- 
-    raise HTTPException( 
-        status_code=status.HTTP_404_NOT_FOUND, 
-        detail="User not found", 
-    )
 
 
