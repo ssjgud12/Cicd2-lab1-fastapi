f11428481e589bd9d66eac785e9f5372bd3a213f
ssjgud12 <150245254+ssjgud12@users.noreply.github.com>
2026-09-18 11:47:01 +0000

    Add readuser

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def health():
 def hello(): 
     return {"message": "Hello from FastAPI"} 
  
- 
+
 @app.post("/api/users", status_code=status.HTTP_201_CREATED) 
 def add_user(new_user: UserCreate): 
     for existing_user in users: 
@@ -28,3 +28,21 @@ def add_user(new_user: UserCreate):
  
     users.append(new_user) 
     return new_user
+
+@app.get("/api/users") 
+def get_users(): 
+    return users 
+ 
+ 
+@app.get("/api/users/{user_id}") 
+def get_user(user_id: int): 
+    for existing_user in users: 
+        if existing_user.user_id == user_id: 
+            return existing_user 
+ 
+    raise HTTPException( 
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="User not found", 
+    )
+
+
