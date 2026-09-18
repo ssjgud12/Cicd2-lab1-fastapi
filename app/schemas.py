32902e1f7688a98712a2059a4b5355fa725b140f
ssjgud12 <150245254+ssjgud12@users.noreply.github.com>
2026-09-18 10:47:11 +0000

    Added schema for UserCreate

---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, Field , StringConstraints
+
+class UserCreate(BaseModel):
+    userid: int = Field(gt=0)
+    name: Annotated[str,StringConstraints(min_length=2, max_length=50)]
+    email:EmailStr
+    age: int = Field(gt=18, lt=120)
+    student_id: Annotated[str,StringConstraints(pattern=r"^S\d(7)$")]
+    
