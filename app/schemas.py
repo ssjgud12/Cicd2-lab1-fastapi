81b6f3c83f7eb58f3886c02340ddfd23bf9f5bb0
ssjgud12 <150245254+ssjgud12@users.noreply.github.com>
2026-09-24 09:17:45 +0000

    Fixed braces causing errors / Test all endpoints

--- app/schemas.py
+++ app/schemas.py
@@ -7,5 +7,6 @@ class UserCreate(BaseModel):
     name: Annotated[str,StringConstraints(min_length=2, max_length=50)]
     email:EmailStr
     age: int = Field(gt=18, lt=120)
-    student_id: Annotated[str,StringConstraints(pattern=r"^S\d(7)$")]
-    
+    student_id: Annotated[str,StringConstraints(pattern=r"^S\d{7}$")]
+
+   
